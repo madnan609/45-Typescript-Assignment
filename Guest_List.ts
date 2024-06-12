35ff4dc79abe432473507dab002cd70fbaeeafde
Muhammad Adnan <126154009+madnan609@users.noreply.github.com>
2024-06-12 22:33:26 +0500

    Add files via upload

--- Guest_List.ts
+++ Guest_List.ts
@@ -1,51 +1,36 @@
 
+
 let Guest_List :string[] = ['Imran Khan','Nawaz Sharif','Asif Ali Zardari'];
+
 // for (let i=0;  i<Guest_List.length; i++){
+
 //      console.log('Dear Mr. ' + Guest_List[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThank You!')
+
 // }
 
 let  absent_Guest :string = 'Imran Khan' ;
+
 let new_Guest :string = 'Kamran Tessori' ;
+
 Guest_List[0] = new_Guest ;
 
-// for (let i=0;  i<Guest_List.length; i++){
-//    console.log('Dear Mr. ' + Guest_List[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThank You!')
-// }
+for (let i=0;  i<Guest_List.length; i++){
 
-// console.log('Mr. ${absent_Guest} is not coming to the party.');
-// console.log('Good News! We find Big Table So we are inviting 3 more guests.')
+    console.log('Dear Mr. ' + Guest_List[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThank You!')
 
-// array me 3 guest add kiye hain.
+}
+
+console.log('Mr. ${absent_Guest} is not coming to the party.');
+
+console.log('Good News! We find Big Table So we are inviting 3 more guests.')
+
 Guest_List.unshift('Sir Zia Khan') ;
 Guest_List.splice(2 ,0, 'Maryam Nawaz');
 Guest_List.push('Bilawal Bhutto Zardari');
 
-// Yahan par men 6 guest ke array ko print kraya he.
-// for (let i=0;  i<Guest_List.length; i++){
-//    console.log('Dear Mr. ' + Guest_List[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThank You!')
-// }
+for (let i=0;  i<Guest_List.length; i++){
 
-// Sorry Message to guest for not inviting.
-// console.log('\nSorry we can not arrange big table, Only Two peoples will be invited.');
+    console.log('Dear Mr. ' + Guest_List[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThank You!')
 
-// Yahan Per mene Guest remove kiya hai. 
-while(Guest_List.length > 2){
-   let remove_Guest = Guest_List.pop();
-//   console.log('Sorry Mr. ${remove_Guest}, You are invited for Dinner.');
 }
 
-// Hamare bache howe 2 Invited Guest.
-// for (let i=0;  i<Guest_List.length; i++){
-//    console.log('Dear Mr. ' + Guest_List[i] + ',\n\nYou are still invited.\n\nThank You!\n\n')
-// }
-
-// Mene sare guest array se remove kar diye.
-Guest_List.splice(0, 2);
-console.log(Guest_List);
-
-
-// Exercise 19
-
-//print a message indicating the number of people you are inviting to dinner.
-
-console.log('Total number of Guest Are: ${Guest_List.length}');
